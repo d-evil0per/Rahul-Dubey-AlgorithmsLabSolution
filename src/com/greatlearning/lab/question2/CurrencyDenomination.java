78480072811fdbb3bf84f3d653d29ec3d2599dfd
D-eviloper <dhir.xcess@gmail.com>
2022-04-11 23:25:49 +0530

    changed formatting

--- src/com/greatlearning/lab/question2/CurrencyDenomination.java
+++ src/com/greatlearning/lab/question2/CurrencyDenomination.java
@@ -7,9 +7,9 @@
 Assume that the denominations are in such a way that any amount can be paid.
  */
 package com.greatlearning.lab.question2;
+
 import java.util.HashMap;
 import java.util.Scanner;
-
 
 /**
  * @author Rahul dubey
@@ -17,54 +17,49 @@ import java.util.Scanner;
  */
 public class CurrencyDenomination {
 
-	
-	
-	
-	public static void countCurrency(int sortedNotes[],int amount)
-	{
+	public static void countCurrency(int sortedNotes[], int amount) {
 
-		int noteArraySize=sortedNotes.length;
+		int noteArraySize = sortedNotes.length;
 		// To Store the Notes as Key and quantity as Value
-        HashMap<Integer,Integer> noteCounter = new HashMap<>();
-      
-        // counting notes 
-        for (int i = 0; i < noteArraySize; i++) {
-            if (amount >= sortedNotes[i]) {
-            	// putting Notes as key and Quotient as value in HashMap  	
-                noteCounter.put(sortedNotes[i], amount / sortedNotes[i]) ;
-                // Calculating the remainder
-                amount = amount % sortedNotes[i];
-            }
-        }
-      
-        // Print notes
-        System.out.println("Your payment approach in order to give min no of notes will be");
-        // It will a sorted dictionary as key is always in ascending order   
-        noteCounter.forEach((key,value)->System.out.println(key+" : "+value));
+		HashMap<Integer, Integer> noteCounter = new HashMap<>();
+
+		// counting notes
+		for (int i = 0; i < noteArraySize; i++) {
+			if (amount >= sortedNotes[i]) {
+				// putting Notes as key and Quotient as value in HashMap
+				noteCounter.put(sortedNotes[i], amount / sortedNotes[i]);
+				// Calculating the remainder
+				amount = amount % sortedNotes[i];
+			}
+		}
+
+		// Print notes
+		System.out.println("Your payment approach in order to give min no of notes will be");
+		// It will a sorted dictionary as key is always in ascending order
+		noteCounter.forEach((key, value) -> System.out.println(key + " : " + value));
 	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MergeSort sort =new MergeSort();
+		MergeSort sort = new MergeSort();
 		try (Scanner sc = new Scanner(System.in)) {
 			System.out.println("Enter the size of currency denominations");
-			int size =sc.nextInt();
-			int notes[]= new int[size];
+			int size = sc.nextInt();
+			int notes[] = new int[size];
 			System.out.println("Enter the currency denominations value");
-			for (int i=0;i<size;i++)
-			{
-				notes[i]=sc.nextInt();
+			for (int i = 0; i < size; i++) {
+				notes[i] = sc.nextInt();
 			}
-			//sorting array using merge sort in descending order
+			// sorting array using merge sort in descending order
 			sort.mergeSort(notes);
-			int [] sortedNotes=sort.getSortedArray();
-			
-			
+			int[] sortedNotes = sort.getSortedArray();
+
 			System.out.println("Enter the amount you want to pay");
-			int amount =sc.nextInt();
+			int amount = sc.nextInt();
 			countCurrency(sortedNotes, amount);
-			
+
 		}
 	}
 
